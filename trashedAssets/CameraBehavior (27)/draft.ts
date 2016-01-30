7cd09c2264ba5eac199c9d479d4f1c10998b3224
Bheesham Persaud <bheesham.persaud@live.ca>
2016-01-30 05:23:27 -0500

    Cleanup.

--- trashedAssets/CameraBehavior (27)/draft.ts
+++
@@ -1,8 +0,0 @@
-class CameraBehavior extends Sup.Behavior {
-  update() {
-    if (Sup.Input.isKeyDown("LEFT")) this.actor.move(-0.1, 0, 0);
-    if (Sup.Input.isKeyDown("RIGHT")) this.actor.move(0.1, 0, 0);
-  }
-}
-
-Sup.registerBehavior(CameraBehavior);
