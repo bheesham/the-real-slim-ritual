beda97f0b67ddc6853045a507c64eb742516ad2b
Bheesham Persaud <bheesham.persaud@live.ca>
2016-01-30 03:48:35 -0500

    Camera stuff works.

--- assets/Act 1 (35)/Areas (36)/1 (37)/Camera Behavior (38)/script.ts
+++ assets/Act 1 (35)/Areas (36)/1 (37)/Camera Behavior (38)/script.ts
@@ -1,16 +1,23 @@
 class CameraBehavior extends Sup.Behavior {
   position: Sup.Math.Vector3 = this.actor.getLocalPosition();
+  cameraActor: Sup.Actor;
+  camera: Sup.Camera;
 
   awake() {
     Game.cameraBehavior = this;
-    
     let cameraManActor = new Sup.Actor("Camera Man");
     new Sup.Camera(cameraManActor);
-    cameraManActor.setPosition(0, 0, 5);
+    
+    this.cameraActor = cameraManActor;
+    this.camera = cameraManActor.camera;
+    
+    cameraManActor.setPosition(-16, -10, 10);
+    cameraManActor.camera.setOrthographicMode(true);
+    cameraManActor.camera.setOrthographicScale(50);
   }
 
   update() {
-    
+    this.position = this.actor.getLocalPosition();
   }
 }
 
--- assets/Game (31)/script.ts
+++ assets/Game (31)/script.ts
@@ -3,8 +3,10 @@ module Game {
         export let playerBehavior: PlayerBehavior;
         
         export function start() {
+          /*
           Sup.loadScene(Sup.get("Scene", Sup.Scene));
           Game.playerBehavior = Sup.getActor("Player").getBehavior(PlayerBehavior);
           Game.cameraBehavior = Sup.getActor("Camera").getBehavior(CameraBehavior);
+          */
         }
 }
